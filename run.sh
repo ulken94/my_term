b8405ef35001a989f6d71a9f8ffce13222afa7db
Jongkuk Lim <lim.jeikei@gmail.com>
2023-09-07 15:52:09 +0900

    Update run.sh
    Update node installation method

--- run.sh
+++ run.sh
@@ -135,10 +135,15 @@ if [ $VIM == "nvim" ]; then
     # Install build prerequisites and node
     if [ $OS == 'Linux' ]; then
       sudo apt-get -y install ninja-build gettext cmake unzip curl file
-      curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
-      sudo bash nodesource_setup.sh
-      rm nodesource_setup.sh
-
+      sudo apt-get update
+      sudo apt-get install -y ca-certificates curl gnupg
+      sudo mkdir -p /etc/apt/keyrings
+      curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+      
+      NODE_MAJOR=16
+      echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+      
+      sudo apt-get update
       sudo apt-get install -y nodejs ripgrep
 
       # Ubuntu needs to install python virtual environment
